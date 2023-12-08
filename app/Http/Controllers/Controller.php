9fa90ae582b67867bcce4c0ba6faad82fe2c465a
Junico Dwi Chandra <junicodwi.chandra@gmail.com>
2023-12-08 21:13:24 +0700

    add swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      title="StarterPack Laravel API",
+ *      version="1.0.0",      
+ *      description="API Documentation",
+ *      @OA\Contact(
+ *          email="junicodwi.chandra@gmail.com",
+ *          name="Junico Dwi Chandra"
+ *      ),
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -10,13 +10,44 @@ class SendEmailController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api');
-    }
+    }    
+
+    /**
+     * @OA\Post(
+     *      path="/api/module/email/send",
+     *      summary="Send Email",
+     *      tags={"Email"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Form send email",
+     *          @OA\JsonContent(
+     *              required={"email"},
+     *              @OA\Property(property="email", type="string"),
+     *              @OA\Property(property="subject", type="string"),
+     *              @OA\Property(property="from", type="string"),
+     *              @OA\Property(property="name", type="string"),
+     *              @OA\Property(property="to", type="array", @OA\Items(type="string")),
+     *              @OA\Property(property="cc", type="array", @OA\Items(type="string")),
+     *              @OA\Property(property="bcc", type="array", @OA\Items(type="string")),
+     *              @OA\Property(property="content", type="string"),
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Failed operation"
+     *      )
+     * )
+     */
     public function send(Request $request){
         try {
             $data = $request->json()->all();
 
             dispatch(new SendEmailJob($data));
-            return response()->json(['message' => 'Send email success!']);
+            return response()->json(['message' => 'Send email success!'], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => 'Error while processing the request', 'error' => $e->getMessage()], 500);
         }
