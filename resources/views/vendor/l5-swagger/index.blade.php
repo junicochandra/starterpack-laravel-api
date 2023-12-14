435b300ebac3e0b6b1d0679ba5b26fa01229ce95
Junico Dwi Chandra <junicodwi.chandra@gmail.com>
2023-12-14 13:18:32 +0700

    update landingpage

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>{{config('l5-swagger.documentations.'.$documentation.'.api.title')}}</title>
+    <title>StarterPack Laravel API</title>
     <link rel="stylesheet" type="text/css" href="{{ l5_swagger_asset($documentation, 'swagger-ui.css') }}">
     <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-32x32.png') }}" sizes="32x32"/>
     <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-16x16.png') }}" sizes="16x16"/>
